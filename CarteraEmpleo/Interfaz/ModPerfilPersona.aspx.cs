7a7be2e617c95555149944256c33b4fb75dac846
Jorge Rojas Aragonés <jeragones@gmail.com>
2013-05-15 04:01:42 -0600

    algo

--- CarteraEmpleo/Interfaz/ModPerfilPersona.aspx.cs
+++ CarteraEmpleo/Interfaz/ModPerfilPersona.aspx.cs
@@ -251,7 +251,7 @@ namespace CarteraEmpleo.Interfaz
 
         protected void btnQuitarIdioma_Click(object sender, EventArgs e)
         {
-            String v = sender.GetType().Name;
+            String v = ClientScript.RegisterClientScriptBlock(this.GetType(),)
             String h = "";
             //DesactivarNombre();
             //DesactivarTelefono();
